537e54eda80eb6fe7ba4aef20fc62e47884dcf49
Yuji Kosugi <yuji.kosugi@gmail.com>
2018-01-09 19:33:32 +0900

    switch to manifest V2 and simplify background.html -> background.js

---
+++ background.js
@@ -0,0 +1,21 @@
+/*	This background page is used to associate a saved session's name
+    to the window it's opened into, so that name can be used as the
+    default name if the user decides to save that window again. */
+
+
+var windowNames = new Array();
+
+/* 	Stores a window's name for the future */
+var setWindowName = function(windowId, name) {
+	windowNames[windowId] = name;
+	console.log(name);
+}
+
+/*	Gets a window's name */
+var getWindowName = function(windowId) {
+	return windowNames[windowId];
+}
+
+/* function removeBoomarks(boookmarkId) {
+	chrome.bookmarks.removeTree(bookmarkId);
+} */
